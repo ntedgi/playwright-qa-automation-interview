ea81c9fb5642af338b64a63026db15bdc7c81c80
Naor Tedgi (Abu Emma) <naor.tedgi@gmail.com>
2025-01-08 14:40:12 +0200

    Update todo-page.ts

--- tests/pages/todo-page.ts
+++ tests/pages/todo-page.ts
@@ -1,62 +1,43 @@
 // todoPage.js
 import {Locator} from "@playwright/test";
+import TodoListComponent from "./components/todo-list-component";
 
 const {expect} = require('@playwright/test');
 
 class TodoPage {
-
-    todoList: Locator
-    item: any;
+    
+    readonly todoListComponent : TodoListComponent
 
     constructor(page) {
         this.page = page;
-        this.todoInput = page.locator('.todo-input'); // Adjusted selector for the todo input field
-        this.todoList = page.locator('.todo-list'); // Selector for list items in the todo list
-        this.item = page.locator('.item'); // Selector for individual todo items
-        this.clearCompletedButton = page.locator('.clear-completed'); // Button to clear completed todos
-        this.undoButton = page.locator('button.undo'); // Adjust based on the actual undo button selector
+        this.todoListComponent = new TodoListComponent(page)
     }
 
     async open() {
-        await this.page.goto('http://localhost:3006');
-        await this.todoInput.waitFor(); // Wait for the input field to ensure the page has loaded
     }
 
     async addTodo(todoText) {
-        await this.todoInput.fill(todoText); // Fill in the text in the todo input
-        await this.todoInput.press('Enter'); // Press Enter to add the todo
     }
 
     async toggleTodo(index) {
-        await this.todoList.nth(index).locator('item').click(); // Selects the checkbox to mark as complete
     }
 
     async deleteTodo(index) {
-        await this.todoList.nth(index).hover();
-        await this.todoList.nth(index).locator('button.destroy').click(); // Adjust if delete button selector differs
     }
 
     async clearCompleted() {
-        await this.clearCompletedButton.click();
     }
 
     async undoLastAction() {
-        await this.undoButton.click(); // Click the undo button
     }
 
-
     async assertTodoText(index, expectedText) {
-        await expect(this.todoList.locator('.item').nth(index)).toContainText(expectedText)
     }
 
     async assertTodoCount(expectedCount) {
-        await expect(this.todoList).toHaveCount(expectedCount);
     }
 
     async assertTodoCompleted(index, completed = true) {
-        const todoItem = this.todoList.nth(index);
-        const isCompleted = await todoItem.locator('input.toggle').isChecked();
-        expect(isCompleted).toBe(completed);
     }
 }
 
